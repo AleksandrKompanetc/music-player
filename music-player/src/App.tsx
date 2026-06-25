04d8fce2cd379cf7aa6d9151e6f4dd96c156a556
AleksandrKompanetc <moneymacker7@gmail.com>
2026-06-25 23:54:37 +0300

    maping tracks

--- music-player/src/App.tsx
+++ music-player/src/App.tsx
@@ -1,25 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   
-  const tracks = []
+  const tracks = [
+    {id: '1', title: 'Musicfun soundtrack', src: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},
+    {id: '2',title: 'Musicfun soundtrack instrumental', src: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'}
+  ]
 
   return (
     <div>
       <h1>Music Player</h1>
       <ul>
-        <li>
-          <div>Musicfun soundtrack</div>
-          <audio src="https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3" controls></audio>
-        </li>
-        <li>
-          <div>Musicfun soundtrack install</div>
-          <audio src="https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3" controls></audio>
-        </li>
+        {tracks.map( t => (
+          <li key={t.id}>
+            <h2>{t.title}</h2>
+            <audio src={t.src} controls></audio>
+          </li>
+        ))}
       </ul>
     </div>
   )
