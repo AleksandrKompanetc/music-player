331e83cb769e93ac22fe256bd510e4e1e959f30c
AleksandrKompanetc <moneymacker7@gmail.com>
2026-06-28 23:24:13 +0300

    Conditional rendering has been implemented.

--- music-player/src/App.tsx
+++ music-player/src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css'
 
-function App() {
-  
-  const tracks = [
+// const tracks = null
+const tracks = [
     {id: '1', title: 'Musicfun soundtrack', src: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},
     {id: '2',title: 'Musicfun soundtrack instrumental', src: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'}
   ]
+
+export function App() {
+
+  if (tracks === null) {
+    return <div>
+      <h1>Music Player</h1>
+      <span>Loading...</span>
+    </div>
+  }
+
+  if (tracks.length === 0) {
+    return <div>
+      <h1>Music Player</h1>
+      <span>No tracks</span>
+    </div>
+  }
 
   return (
     <div>
