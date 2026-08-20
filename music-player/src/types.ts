f066eece3defbf6dffe8006e0b50a63ae7bc5c68
AleksandrKompanetc <moneymacker7@gmail.com>
2026-08-20 23:14:17 +0300

    changed and added types

--- music-player/src/types.ts
+++ music-player/src/types.ts
@@ -1,7 +1,29 @@
+export interface TrackArtwork {
+  '150x150': string
+  '480x480': string
+  '1000x1000': string
+}
+
+export interface TrackUser {
+  id: number
+  name: string
+  handle: string
+}
+
 export interface Track {
   userId: number
   id: number
   title: string
   body: string
-  description?: string
+  description?: string | null
+  genre: string
+  mood: string | null
+  duration: number
+  play_count: number
+  repost_count: number
+  favorite_count: number
+  artwork: TrackArtwork | null
+  user: TrackUser
+  permalink: string
+  created_at: string
 }
