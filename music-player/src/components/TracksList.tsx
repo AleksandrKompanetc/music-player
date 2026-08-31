75a82dc3e710c4fd168fcf28d94962d29472e787
AleksandrKompanetc <moneymacker7@gmail.com>
2026-08-31 23:44:17 +0300

    added TrackListProps for TrackList

--- music-player/src/components/TracksList.tsx
+++ music-player/src/components/TracksList.tsx
@@ -1,17 +1,23 @@
 import { Track } from '../types'
 
-export function TrackList({
+interface TrackListProps {
+  tracks: Track[]
+  selectedTrackId: string | null
+  onSelectTrack: (track: Track) => void
+  loading: boolean
+  error: string | null
+}
+
+export default function TrackList({
   tracks,
   selectedTrackId,
   onSelectTrack,
   loading,
   error,
-}) {
+}: TrackListProps) {
   return (
     <ul>
-      {tracks.map((track) => {
-        
-      })}
+      
     </ul>
 
   )
