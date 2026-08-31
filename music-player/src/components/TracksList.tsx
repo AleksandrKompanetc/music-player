9ac0960335cd93f0a8eb0ea716df7a0d48d60f15
AleksandrKompanetc <moneymacker7@gmail.com>
2026-08-31 23:33:20 +0300

    refactor TrackList component: add props for selected track, selection handler, loading state, and error handling

--- music-player/src/components/TracksList.tsx
+++ music-player/src/components/TracksList.tsx
@@ -2,9 +2,17 @@ import { Track } from '../types'
 
 export function TrackList({
   tracks,
-
+  selectedTrackId,
+  onSelectTrack,
+  loading,
+  error,
 }) {
   return (
-    
+    <ul>
+      {tracks.map((track) => {
+        
+      })}
+    </ul>
+
   )
 }
