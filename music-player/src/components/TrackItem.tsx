090d2b66eaed2d4f068980e887c05dfc923ba9c2
AleksandrKompanetc <moneymacker7@gmail.com>
2026-09-19 23:54:38 +0300

    created   UserItem component

--- music-player/src/components/TrackItem.tsx
+++ music-player/src/components/TrackItem.tsx
@@ -1,3 +1,21 @@
+
+
+const UserItem = ({ user }) => {
+  return (
+    <div>
+      <h3>{user.name}</h3>
+      <p>{user.email}</p>
+  )
+}
+
+export default UserItem
+
+
+
+
+
+
+
 // import type { Track } from '../types'
 
 // interface TrackItemProps {
