5c615e16ba1fce913c50fb18d4dbce1532d76cbf
AleksandrKompanetc <moneymacker7@gmail.com>
2026-09-19 23:46:45 +0300

    created UserList component

--- music-player/src/components/TrackList.tsx
+++ music-player/src/components/TrackList.tsx
@@ -1,3 +1,29 @@
+import UserItem from './UserItem'
+
+const UserList = ({ users }) => {
+  return (
+    <div>
+      {users.map((user) => (
+        <UserItem
+          key={user.id}
+          user={user}
+          
+        />
+      ))}
+    </div>
+  )
+}
+
+export default UserList
+
+
+
+
+
+
+
+
+
 // import React from 'react'
 // import type { Track } from '../types'
 // import TrackItem from './TrackItem'
